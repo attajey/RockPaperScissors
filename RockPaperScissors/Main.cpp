9b93dadcdf6e3a3a3ee738135b61f1bccaf7c2ae
Atta Jirofty <jirofty76@gmail.com>
2022-06-22 19:34:37 -0400

    Greetings added. Comments in Main added

---
+++ RockPaperScissors/Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string userName;
+	cout << "GREETINGS! WLC TO R-P-S Game" << endl;
+	cout << "Please Enter Your Username : ";
+	cin >> userName;
+
+	// 1. Search through files to find userName.txt
+
+	// if exists, read 2 numbers : player wins AND computer wins
+	// if not exists, create one
+
+	// 3. Computer randomly pick R-P-S
+
+	// 4. Ask user to choose 1=R - 2=P - 3=S
+
+	// 5. See who won
+
+	// 6. Display the game result to player
+
+	// 7. Keep track of "wins by the player" and "wins by the computer" 
+
+	// 8. Ask player if want to play again : Y - N 
+	// if Y : Go to step 3
+	// if N : continue to step 9
+
+	// 9. save in userName.txt
+
+	// 10. Privide confirmation msg to player
+
+	// 11. End the game
+
+	return 0;
+}
