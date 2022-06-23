f9c968b479f4c1ed9e69c038b84dd6f7e8308bba
Atta Jirofty <jirofty76@gmail.com>
2022-06-22 20:24:25 -0400

    First Draft. For sure it break !

--- RockPaperScissors/Main.cpp
+++ RockPaperScissors/Main.cpp
@@ -9,12 +9,14 @@ int main()
 {
 	fstream userFile;
 	string userName;
-	int playerWins;
-	int computerWins;
-	int playerChoiceIndex;
+	int playerWins = 0;
+	int computerWins = 0;
+	int playerChoiceIndex = 0;
 	string playerChoice;
 	string gameOptions[] = { "Rock", "Paper", "Scissors" };
 	srand(time(NULL));
+	bool endGame = false;
+	bool playerWinFlag = false;
 
 	cout << "GREETINGS! WLC TO R-P-S Game" << endl;
 	cout << "Please Enter Your Username : ";
@@ -22,16 +24,21 @@ int main()
 	//userFile << userName; // write in file 
 
 	// 1. Search through files to find userName.txt
-	userFile.open(userName + ".txt", ios::app);
+	userFile.open(userName + ".txt", ios::in);
 	if (userFile)
 	{
 		if (userFile.is_open())
 		{
-			
-		}
-	// if exists, read 2 numbers : player wins AND computer wins
 		userFile >> playerWins;
 		userFile >> computerWins;
+		userFile.close();
+			
+		}
+		else
+		{
+			cerr << "Unable to open File ! " << endl;
+		}
+	// if exists, read 2 numbers : player wins AND computer wins
 
 	}
 	else
@@ -40,86 +47,119 @@ int main()
 
 	}
 
-
-	// 3. Computer randomly pick R-P-S
-	string computerRandomChoice = gameOptions[rand() % 3];
-	//cout << computerRandomChoice;
-	
-
-
-	// 4. Ask user to choose 1=R - 2=P - 3=S
-	cout << "Please Choose One of the Following : 1=Rock  2=Paper  3=Scissors" << endl;
-	cin >> playerChoiceIndex;
-	playerChoice = gameOptions[playerChoiceIndex - 1];
-
-
-
-
-	// 5. See who won
-	if (computerRandomChoice == "Rock")
+	while (!endGame)
 	{
-		if (playerChoice == "Rock")
+		// 3. Computer randomly pick R-P-S
+		string computerRandomChoice = gameOptions[rand() % 3];
+		//cout << computerRandomChoice;
+
+		// 4. Ask user to choose 1=R - 2=P - 3=S
+		cout << "Please Choose One of the Following : 1=Rock  2=Paper  3=Scissors" << endl;
+		cin >> playerChoiceIndex;
+		playerChoice = gameOptions[playerChoiceIndex - 1];
+
+		// 5. See who won
+		if (computerRandomChoice == "Rock")
 		{
-		//Draw
+			if (playerChoice == "Rock")
+			{
+				//Draw
+				continue;
+
+			}
+			else if (playerChoice == "Paper")
+			{
+				// Player Wins
+				playerWins++;
+				playerWinFlag = true;
+			}
+			else if (playerChoice == "Scissors")
+			{
+				// Computer Wins
+				computerWins++;
+				playerWinFlag = false;
+			}
 
 		}
-		else if (playerChoice == "Paper")
+		else if (computerRandomChoice == "Paper")
 		{
-			// Player Wins
+			if (playerChoice == "Rock")
+			{
+				// Computer Wins
+				computerWins++;
+				playerWinFlag = false;
+			}
+			else if (playerChoice == "Paper")
+			{
+				// Draw
+				continue;
+			}
+			else if (playerChoice == "Scissors")
+			{
+				// Player Wins
+				playerWins++;
+				playerWinFlag = true;
+			}
 		}
-		else if (playerChoice == "Scissors")
+		else if (computerRandomChoice == "Scissors")
 		{
-			// Computer Wins
+			if (playerChoice == "Rock")
+			{
+				// Player Wins
+				playerWins++;
+				playerWinFlag = true;
+			}
+			else if (playerChoice == "Paper")
+			{
+				// Computer Wins
+				computerWins++;
+				playerWinFlag = false;
+
+			}
+			else if (playerChoice == "Scissors")
+			{
+				// Draw
+				continue;
+			}
 		}
 
-	}
-	else if (computerRandomChoice == "Paper")
-	{
-		if (playerChoice == "Rock")
+
+
+
+		// 6. Display the game result to player
+		if (playerWinFlag)
 		{
-			// Computer Wins
+			cout << "YOU HAVE WON !!! YEAYYYY" << endl;
 		}
-		else if (playerChoice == "Paper")
+		else
 		{
-			// Draw
+			cout << "L   LOOSER   L" << endl;
 		}
-		else if (playerChoice == "Scissors")
+
+		// 7. Keep track of "wins by the player" and "wins by the computer" 
+		userFile.open(userName + ".txt", ios::out || fstream::trunc);
+		userFile << playerWins;
+		userFile << computerWins;
+
+		//userFile << userName;
+
+		// 8. Ask player if want to play again : Y - N 
+		cout << "Do you want to play again ? (1=Y / N=0)" << endl;
+		cin >> endGame;
+
+		// if Y : Go to step 3
+		if (endGame)
 		{
-			// Player Wins
+		// 9. save in userName.txt
+			userFile.close();
+			// 10. Privide confirmation msg to player
+			cout << "Your Score was saved successfully. See ya soon ! " << endl;
+			// 11. End the game
+			break;
 		}
-	}
-	else if (computerRandomChoice == "Scissors")
-	{
-		if (playerChoice == "Rock")
-		{
-			// Player Wins
-		}
-		else if (playerChoice == "Paper")
-		{
-			// Computer Wins
-		}
-		else if (playerChoice == "Scissors")
-		{
-			// Draw
-		}
-	}
-	
 
 
 
-	// 6. Display the game result to player
-
-	// 7. Keep track of "wins by the player" and "wins by the computer" 
-
-	// 8. Ask player if want to play again : Y - N 
-	// if Y : Go to step 3
-	// if N : continue to step 9
-
-	// 9. save in userName.txt
-
-	// 10. Privide confirmation msg to player
-
-	// 11. End the game
-
+	} // if N : continue to step 9
 	return 0;
 }
